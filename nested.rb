d0ef821f40bdd5389896ec219b5d00104645cbc6
Christina Park <christinapark0117@gmail.com>
2019-09-14 00:40:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -67,9 +67,6 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-  programmer_hash[:yukihiro_matsumoto][:languages] =["LISP", "C"]
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -85,7 +82,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-     
+  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+  programmer_hash[:yukihiro_matsumoto][:languages] =["LISP", "C"]   
 end
 
 def changing_alan
